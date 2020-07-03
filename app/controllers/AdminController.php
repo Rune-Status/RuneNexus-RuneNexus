3605e8ef86ee0c66f67176a171d1ffc478b93434
KingFox <rune.evo2012@gmail.com>
2020-07-02 22:12:13 -0500

    Changed admin chart to show votes instead

--- app/controllers/AdminController.php
+++ app/controllers/AdminController.php
@@ -19,23 +19,18 @@ class AdminController extends Controller {
             ]
         ];
 
-        $dates    = $this->getChartDates(30);
-        
-        $payments = Payments::getChartData($dates);
-        $members  = Users::getChartData($dates);
-        $servers  = Servers::getChartData($dates);
+        $dates = $this->getChartDates(14);
+        $votes = Votes::getChartData($dates);
 
         $this->set("chart_keys", array_keys($dates['chart']));
-        $this->set("payment_data", $payments);
-        $this->set("members_data", $members);
-        $this->set("servers_data", $servers);
+        $this->set("votes_data", $votes);
         $this->set("data", $data);
         return true;
     }
 
     public function getChartDates($dayLimit = 14, $format = "m.d") {
-        $start = strtotime(date("Y-m-d 00:00:00")." -$dayLimit days");
-        $end   = strtotime(date("Y-m-d 23:59:59"));
+        $start = time() - (86400 * $dayLimit);
+        $end   = time();
 
         $data = [
             'start'  => $start,
@@ -43,10 +38,11 @@ class AdminController extends Controller {
             'chart'  => [],
         ];
 
-        while($start < $end) {
-            $start += 86400; // increment by 1 day until we reach today
+        while($start <= $end) {
             $date = date($format, $start);
             $data['chart'][$date] = 0;
+
+            $start += 86400; // increment by 1 day until we reach today
         }
 
         return $data;
