921d4b2158eaf4b8e3ddd7cf9c16c3ab3109b757
KingFox <rune.evo2012@gmail.com>
2020-09-09 18:03:17 -0500

    Fixed broken profile

--- app/controllers/ProfileController.php
+++ app/controllers/ProfileController.php
@@ -9,12 +9,14 @@ class ProfileController extends Controller {
         $idArr = array_column($servers->toArray(), "id");
         $ids   = implode(",", $idArr);
 
-        $votes = Votes::
-            select("*")
-            ->whereRaw("server_id IN (".$ids.")")
-            ->leftJoin("servers", "servers.id", "=", "votes.server_id")
-            ->orderByRaw("votes.voted_on DESC")
-            ->get();
+        if (count($servers) > 0) {
+            $votes = Votes::
+                select("*")
+                ->whereRaw("server_id IN (".$ids.")")
+                ->leftJoin("servers", "servers.id", "=", "votes.server_id")
+                ->orderByRaw("votes.voted_on DESC")
+                ->get();
+        }
 
         $votesArr = [];
 
@@ -29,29 +31,29 @@ class ProfileController extends Controller {
             ];
         }
 
-        foreach($votes as $vote) {
-            $vote_time = $vote->voted_on;
-            $timeDiff  = time() - $vote_time;
+        if (count($servers) > 0) {
+            foreach($votes as $vote) {
+                $vote_time = $vote->voted_on;
+                $timeDiff  = time() - $vote_time;
 
-            if ($timeDiff <= 3600) {
-                $votesArr[$vote->server_id]['1hour']++;
-            } 
-            if ($timeDiff <= 86400) {
-                $votesArr[$vote->server_id]['1day']++;
-            }  
-            if ($timeDiff <= 604800) {
-                $votesArr[$vote->server_id]['7days']++;
-            }  
-            if ($timeDiff <= 2592000) {
-                $votesArr[$vote->server_id]['30days']++;
-            }  
-            if ($timeDiff <= 10368000) {
-                $votesArr[$vote->server_id]['60days']++;
-            } 
-
-            $votesArr[$vote->server_id]['lifetime']++;
+                if ($timeDiff <= 3600) {
+                    $votesArr[$vote->server_id]['1hour']++;
+                } 
+                if ($timeDiff <= 86400) {
+                    $votesArr[$vote->server_id]['1day']++;
+                }  
+                if ($timeDiff <= 604800) {
+                    $votesArr[$vote->server_id]['7days']++;
+                }  
+                if ($timeDiff <= 2592000) {
+                    $votesArr[$vote->server_id]['30days']++;
+                }  
+                if ($timeDiff <= 10368000) {
+                    $votesArr[$vote->server_id]['60days']++;
+                } 
+                $votesArr[$vote->server_id]['lifetime']++;
+            }
         }
-
         $this->set("roles", $roles);
         $this->set("servers", $servers);
         $this->set("voteData", $votesArr);
